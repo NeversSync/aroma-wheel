6f68c3c25de5dcf49d789cd0c5abe199ac0550f4
nicky evers <livelyevers@gmail.com>
2017-07-29 11:15:54 -0700

    boiler plate and dependencies installed

---
+++ public/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to the Quantitea Aroma Wheel</h2>
+        </div>
+        <p className="App-intro">
+          Bommmbbbb teaaaaaaa!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
